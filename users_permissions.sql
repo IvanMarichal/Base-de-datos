262962686a3bfdc145f80233ec2a656f9e52f1ff
RodriGarcia12 <66884196+RodriGarcia12@users.noreply.github.com>
2022-08-31 14:55:00 -0300

    nuevo usuario y sus respectivos permisos
    se agrego al usuario apiclient@localhost y los permisos que tendra el mismo

--- users_permissions.sql
+++ users_permissions.sql
@@ -19,6 +19,16 @@ FLUSH PRIVILEGES;
 REVOKE SELECT ON user FROM 'apiresults'@localhost;
 REVOKE SELECT ON client FROM 'apiresults'@localhost;
 REVOKE SELECT ON card FROM 'apiresults'@localhost;
+REVOKE SELECT ON subscription FROM 'apiresults'@localhost;
+REVOKE SELECT ON client_card FROM 'apiresults'@localhost;
+REVOKE SELECT ON card_number_card FROM 'apiresults'@localhost;
+REVOKE SELECT ON subscription_type FROM 'apiresults'@localhost;
+REVOKE SELECT ON client_fav_teams FROM 'apiresults'@localhost;
+REVOKE SELECT ON client_fav_events FROM 'apiresults'@localhost;
+REVOKE SELECT ON client_fav_league FROM 'apiresults'@localhost;
+REVOKE SELECT ON client_fav_events FROM 'apiresults'@localhost;
+REVOKE SELECT ON client_fav_tournament FROM 'apiresults'@localhost;
+REVOKE SELECT ON ads FROM 'apiresults'@localhost;
 
 CREATE USER 'apiads'@localhost IDENTIFIED BY 'ApiAdsMysql1!';
 GRANT SELECT, INSERT, UPDATE ON ads TO 'apiads'@localhost;
@@ -27,6 +37,18 @@ CREATE USER 'backoffice'@localhost IDENTIFIED BY 'BackofficeMysql1!';
 GRANT SELECT, INSERT, UPDATE ON avocado_database.* TO 'backoffice'@localhost;
 FLUSH PRIVILEGES;
 REVOKE SELECT, INSERT, UPDATE ON user FROM 'backoffice'@localhost;
+
+CREATE USER 'apiclient'@localhost IDENTIFIED BY 'ApiClientMysql1!';
+GRANT SELECT, INSERT, UPDATE ON client TO ‘apiclient’@localhost;
+GRANT SELECT, INSERT, UPDATE ON client_fav_teams TO ‘apiclient’@localhost;
+GRANT SELECT, INSERT, UPDATE ON client_fav_events TO ‘apiclient’@localhost;
+GRANT SELECT, INSERT, UPDATE ON client_fav_league TO ‘apiclient’@localhost;
+GRANT SELECT, INSERT, UPDATE ON client_fav_tournament TO ‘apiclient’@localhost;
+GRANT SELECT, INSERT, UPDATE ON client_avatar_link TO ‘apiclient’@localhost;
+GRANT SELECT, INSERT, UPDATE ON card TO ‘apiclient’@localhost;
+GRANT SELECT, UPDATE ON card_number TO ‘apiclient’@localhost;
+GRANT SELECT, UPDATE ON client_card TO ‘apiclient’@localhost;
+GRANT SELECT ON subscription TO ‘apiclient’@localhost;
 
 
 
